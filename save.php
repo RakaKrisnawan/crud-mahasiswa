34338fc6a323a63f0ff0e4b4075e1f2e63bbbf54
RakaKrisnawan <rakakrisnawan25@gmail.com>
2025-11-24 14:34:07 +0800

    Remove unused root files (testing.php, save.php, members.php, update.php, delete.php, edit.php, create.php)

--- save.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// require necessary files
-
-// check if user is logged in
-
-// handle form submission
--- update.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// require necessary files
-
-// check if user is logged in
-
-// handle form submission
-
-// redirect back to members if accessed directly
